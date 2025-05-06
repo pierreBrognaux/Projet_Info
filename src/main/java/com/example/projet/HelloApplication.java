373a54a54e5ab730cf42db6e4f368a338b176b64
martin <martin.decompoix@edu.ece.fr>
2025-05-06 11:26:22 +0200

    test ecran rouge

--- src/main/java/com/example/projet/HelloApplication.java
+++ src/main/java/com/example/projet/HelloApplication.java
@@ -1,18 +1,19 @@
 package com.example.projet;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 1920);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: red;");
+
+        Scene scene = new Scene(root, 400, 300);
+        stage.setTitle("Fenêtre Rouge");
         stage.setScene(scene);
         stage.show();
     }
